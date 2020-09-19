2d5d128bb67add9ad6f2a618f5b67e06203396ec
“masa-b24” <take3611.pg@gmail.com>
2020-09-19 15:51:51 +0900

    maypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'static_pages#home'
+  devise_for :users, controllers: {   registrations: 'users/registrations', sessions: 'users/sessions' }
 
+  resources :users, :only => [:show] do
+    resources :tasks, :only => [:show, :new, :edit]
+  end
+  
+  root to: 'static_pages#home'
+  
 end
